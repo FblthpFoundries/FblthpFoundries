c384b8a0a5b6cda36ecf23337db902fa75b6c72d
OWEN JACKSON <u1229070@umail.utah.edu>
2024-02-28 20:19:34 -0700

    Actually got init to build CSV from scryfall

--- fblthp/init.py
+++ fblthp/init.py
@@ -1,2 +1,34 @@
 import json
+import requests
 
+r = requests.get('https://api.scryfall.com/bulk-data/oracle-cards')
+
+assert(r.status_code == 200)
+
+r.close()
+
+jsonURI = json.loads(r.content)['download_uri']
+
+r = requests.get(jsonURI)
+
+cards = json.loads(r.content)
+
+#https://scryfall.com/docs/api/cards Currently only loading creatures
+
+features = ['mana_cost', 'name', 'type_line', 'power', 'toughness', 'oracle_text', 'flavor_text']
+
+data = ""
+for feature in features:
+    data += feature + ','
+data = data[:-1]
+
+for card in cards:
+    if 'Creature' not in card['type_line']:
+        continue
+    data += '\n'
+    for feature in features:
+        if feature not in card:
+            data += '<empty>,'
+            continue
+        data += '\"' + card[feature] + '\",'
+    data = data[:-1]

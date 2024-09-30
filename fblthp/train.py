3b6bdb65d812d637e91bc2d18e057da8f306f6a2
sfarrell0 <samfarrell.cs@gmail.com>
2024-09-29 19:18:43 -0600

    Magic Dataset for TransformerVAE

--- fblthp/train.py
+++ fblthp/train.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader, Dataset
 from TransformerVAE import TransformerVAE
 from tokenizers import Tokenizer, models, trainers, pre_tokenizers
 from tqdm import tqdm  # Import tqdm for progress bars
-import os
+import json, requests, os
 import pandas
 
 # Dummy dataset for demonstration purposes
@@ -22,8 +22,83 @@ class RandomDataset(Dataset):
         # Random sequence of token IDs
         torch.manual_seed(idx)
         return torch.randint(0, self.vocab_size, (self.seq_length,))
-class MagicCardDatasetProcessor():
-    def __init__(self):
+class MagicCardDataset(Dataset):
+    def __init__(self, tokenizer, max_len=200):
+
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        if not os.path.exists('cards.csv'):
+            self.pull_scryfall_to_csv('cards.csv')
+        if not os.path.exists('corpus.csv'):
+            self.prepare_corpus('cards.csv', 'corpus.csv')
+
+        self.corpus_dataframe = pandas.read_csv('corpus.csv')
+
+    def __len__(self):
+        return len(self.corpus_dataframe)
+
+    def __getitem__(self, idx):
+        card_text = self.corpus_dataframe.iloc[idx]['card']
+
+        tokenized = self.tokenizer.encode(card_text).ids
+
+        return torch.tensor(tokenized)    
+
+    def pull_scryfall_to_csv(self, filename):
+        r = requests.get('https://api.scryfall.com/bulk-data/oracle-cards')
+
+        assert(r.status_code == 200)
+
+        r.close()
+
+        jsonURI = json.loads(r.content)['download_uri']
+
+        r = requests.get(jsonURI)
+
+        cards = json.loads(r.content)
+        r.close()
+
+        #https://scryfall.com/docs/api/cards Currently only loading creatures
+
+        features = ['mana_cost', 'name', 'type_line', 'power', 'toughness', 'oracle_text', 'loyalty', 'flavor_text']
+
+        cardNums = len(cards)
+
+        num = 0
+
+        f = open(filename, 'w', encoding='utf-8')
+
+        data = ""
+        for feature in features:
+            data += feature + ','
+        data = data[:-1]
+
+        f.write(data)
+
+        for card in cards:
+            num  += 1
+            if  'Token' in card['type_line'] or 'card_faces' in card:
+                continue
+            if not 'paper' in card['games']:
+                continue
+            if 'Hero' in card['type_line'] or 'Plane' in card['type_line'] or 'Card' in card['type_line']:
+                continue
+            data = '\n'
+            for feature in features:
+                if feature not in card:
+                    data += '<empty>,'
+                    continue
+                data += '\"' + card[feature].replace("\"", "").replace('\n', ' <nl> ').replace('}{', '} {') + '\",'
+            data = data[:-1]
+            f.write(data)
+
+            if num % 1000 == 0:
+                print(f'{num}/{cardNums}')
+
+        f.close()
+
+    def prepare_corpus(self, cards_csv, filename):
+        df = pandas.read_csv(cards_csv)
         self.featureDict ={
         'type_line': '<tl>',
         'name': '<name>',
@@ -55,25 +130,6 @@ class MagicCardDatasetProcessor():
         self.xx = '[0-9|X]+/[0-9|X]+'
         self.pm = '\\+[0-9|X]+/\\-[0-9|X]+'
         self.mp = '\\-[0-9|X]+/\\+[0-9|X]+'
-
-    def sanitize(self, text):
-        while '{' in text:
-            indexOpen  = text.index('{')
-            indexClose = text.index('}')
-
-            symbol =text[indexOpen: indexClose + 1]
-            token = '<' + text[indexOpen + 1: indexClose] + '>'
-
-            if not symbol in self.specialTokenDict:
-                self.specialTokenDict[symbol] = token
-
-            text = text[: indexOpen] + token + text[indexClose + 1:]
-
-        return text
-
-    def prepare_corpus(self, csv):
-        df = pandas.read_csv(csv)
-
         
 
         corpus = []
@@ -88,9 +144,9 @@ class MagicCardDatasetProcessor():
                 if not feature == 'name':
                     append = append.replace(name, '~')
                 text +=  append + ' ' + self.featureDict[feature][:1] + '\\' + self.featureDict[feature][1:]
-            corpus.append(self.sanitize(text[1:]) + self.specialTokenDict['eos'])
+            corpus.append(text[1:] + self.specialTokenDict['eos'])
 
-        f = open('corpus.csv', 'w', encoding='utf-8')
+        f = open(filename, 'w', encoding='utf-8')
         f.write('card\n')
         for text in corpus:
             f.write('\"'+text + '\"\n')
@@ -140,13 +196,11 @@ def main():
     num_heads = 1  # Number of attention heads
     hidden_dim = 256  # Feedforward network hidden dimension
     num_layers = 1  # Number of transformer encoder/decoder layers
-    max_len = 100  # Maximum length of sequences
+    max_len = 200  # Maximum length of sequences
     batch_size = 4
     num_epochs = 4
     learning_rate = 0.001
 
-    processor = MagicCardDatasetProcessor()
-    corpus = processor.prepare_corpus('cards.csv')
 
     vocab_size = 20000
     tokenizer_exists = False
@@ -155,13 +209,15 @@ def main():
         tokenizer = Tokenizer(models.WordPiece())
         tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
         special_tokens = ["<pad>", "<sos>", "<eos>", "<unk>"]
+        tokenizer.enable_padding(pad_id=0, pad_token=special_tokens[0], length=max_len)
+        tokenizer.enable_truncation(max_length=max_len)
         trainer = trainers.WordPieceTrainer(vocab_size=vocab_size, special_tokens=special_tokens)
         tokenizer.train(['corpus.csv'], trainer)
         tokenizer.save("wordpiece_tokenizer.json")
     else:
         tokenizer = Tokenizer.from_file("wordpiece_tokenizer.json")
 
-
+    
     # Create the VAE model
     vae_model = TransformerVAE(vocab_size, embed_dim, num_heads, hidden_dim, num_layers, max_len)
     vae_model.print_model_size()
@@ -172,7 +228,7 @@ def main():
     optimizer = optim.Adam(vae_model.parameters(), lr=learning_rate)
     
     # DataLoader (use a dummy dataset for now)
-    dataset = RandomDataset(num_samples=1000, seq_length=max_len, vocab_size=vocab_size)
+    dataset = MagicCardDataset(tokenizer, max_len=max_len)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
     # Start training
